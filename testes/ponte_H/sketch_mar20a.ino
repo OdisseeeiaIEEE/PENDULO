858dff3a3f0b366015a7367675057e2edd70a8e6
Wesley Cantarino <32984446+wesley-cantarino@users.noreply.github.com>
2019-03-20 16:23:05 -0300

    Update sketch_mar20a.ino

--- testes/ponte_H/sketch_mar20a.ino
+++ testes/ponte_H/sketch_mar20a.ino
@@ -1,24 +1,52 @@
+const int set1 = 16;
+const int set2 = 5;
+const int pwm = 4;
+
 const int POT = A0;
-int ledPIN = 16;
+
 void setup()
 {
   Serial.begin(115200);
-  pinMode (ledPIN, OUTPUT);
+
+  pinMode(set1, OUTPUT);
+  pinMode(set2, OUTPUT);
+  pinMode(pwm, OUTPUT);
 }
 
 void loop()
 {
+  /*
+  digitalWrite(set1, HIGH);
+  digitalWrite(set2, LOW);
+
+  digitalWrite(pwm, HIGH);
+  */
+    
+  
   float pot = analogRead(POT);
-  if (pot > 512){
-  digitalWrite(ledPIN, HIGH);
-  delay (100);
-  //digitalWrite(ledPIN, LOW);
- //delay (100);
-  }
-  else {
-    digitalWrite(ledPIN, LOW);
-    delay(100);
-  }
   Serial.println(pot);
- // delay(100);
+  
+  if (pot > 600)
+  {
+    digitalWrite(set1, HIGH);
+    digitalWrite(set2, LOW);
+
+    //analogWrite(pwm, 120);
+    digitalWrite(pwm, HIGH);
+  }
+  else if (pot < 400)
+  {
+    digitalWrite(set1, LOW);
+    digitalWrite(set2, HIGH); 
+
+    //analogWrite(pwm, 120);
+    digitalWrite(pwm, HIGH);
+  }
+  else
+  {
+    digitalWrite(set1, LOW);
+    digitalWrite(set2, LOW);
+
+    digitalWrite(pwm, LOW);
+  }
 }
