221d906c718cb2ce37ae0e9e428a7e7b76e25757
Mateus Francisco Eugenio <46822776+MateusFEugenio@users.noreply.github.com>
2019-03-23 13:15:50 -0300

    Update sketch_mar20a.ino

--- testes/ponte_H/sketch_mar20a.ino
+++ testes/ponte_H/sketch_mar20a.ino
@@ -1,29 +1,21 @@
-const int set1 = 16;
-const int set2 = 5;
-const int pwm = 4;
+const int set1 = 4;
+const int set2 = 3;
+const int pwm = 5;
 
-const int POT = A0;
+const int POT = 8;
 
-void setup()
+void setup ()
 {
   Serial.begin(115200);
-
+  
   pinMode(set1, OUTPUT);
   pinMode(set2, OUTPUT);
   pinMode(pwm, OUTPUT);
 }
 
-void loop()
+void loop ()
 {
-  /*
-  digitalWrite(set1, HIGH);
-  digitalWrite(set2, LOW);
-
-  digitalWrite(pwm, HIGH);
-  */
-    
-  
-  float pot = analogRead(POT);
+  int pot = analogRead(POT);
   Serial.println(pot);
   
   if (pot > 600)
