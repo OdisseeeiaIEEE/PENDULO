90be4321278182513440eaccc00f81f3b5d2f735
Wesley Cantarino <32984446+wesley-cantarino@users.noreply.github.com>
2019-03-23 14:25:07 -0300

    ponte H variação
    variação gradual do motor usando aquela clássica ponte h vermelhinha com potenciômetro.

--- testes/ponte_H/sketch_mar20a.ino
+++ testes/ponte_H/sketch_mar20a.ino
@@ -1,8 +1,10 @@
 const int set1 = 4;
 const int set2 = 3;
+
 const int pwm = 5;
 
-const int POT = 8;
+const int POT = A8;
+int pot = 0;
 
 void setup ()
 {
@@ -11,34 +13,44 @@ void setup ()
   pinMode(set1, OUTPUT);
   pinMode(set2, OUTPUT);
   pinMode(pwm, OUTPUT);
+
+  digitalWrite(pwm, HIGH);
 }
 
 void loop ()
 {
-  int pot = analogRead(POT);
-  Serial.println(pot);
+  pot = analogRead(POT);
+  //Serial.println(pot);
   
   if (pot > 600)
   {
-    digitalWrite(set1, HIGH);
+    //digitalWrite(set1, HIGH);
+    int val = map(pot, 600, 750, 10, 255);
+    val = constrain(val, 0, 255);
+    Serial.println(val);
+    
+    analogWrite(set1, val);
+    
     digitalWrite(set2, LOW);
 
-    //analogWrite(pwm, 120);
-    digitalWrite(pwm, HIGH);
+    Serial.println("set1");
   }
-  else if (pot < 400)
+  else if (pot < 300)
   {
     digitalWrite(set1, LOW);
-    digitalWrite(set2, HIGH); 
 
-    //analogWrite(pwm, 120);
-    digitalWrite(pwm, HIGH);
+    int val = map(pot, 0, 300, 255, 10);
+    val = constrain(val, 0, 255);
+    analogWrite(set2, val);
+    //digitalWrite(set2, HIGH); 
+
+    Serial.println("set2");
   }
   else
   {
     digitalWrite(set1, LOW);
     digitalWrite(set2, LOW);
 
-    digitalWrite(pwm, LOW);
+    Serial.println("desligado");
   }
 }
